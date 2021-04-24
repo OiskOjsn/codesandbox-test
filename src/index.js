15c3308799c9b9bfe11ab2102795c3d411562ea6
OiskOjsn <70461823+OiskOjsn@users.noreply.github.com>
2021-04-24 23:01:49 +0900

    standard

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 console.clear();
 
-const num = 1300;
-console.log(num.toLocaleString());
+// const num = 1300;
+// console.log(num.toLocaleString());
 
-const formattedNum =
-  typeof num === "number" ? num.toLocaleString() : "数値を入力してね";
-console.log(formattedNum);
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してね";
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
+// };
+// console.log(checkSum(50, 60));
+
+// const flag1 = true;
+// const flag2 = true;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はTrue");
+// }
+
+// if (flag1 && flag2) {
+//   console.log("1も2もTrue");
+// }
+
+// const num = "bom";
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// const num2 = 1;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
